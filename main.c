604d0585a6f5b2e10d78a0a55c6418d0a6df43a1
Blake Eastman <bt.eastman@Yahoo.com>
2017-10-18 15:25:43 -0400

    reconfigured for Jordan's robot

--- main.c
+++ main.c
@@ -1,32 +1,13 @@
-#pragma config(Motor,  port2,           backleft,    tmotorNormal, openLoop)
-#pragma config(Motor,  port3,           frontleft,     tmotorNormal, openLoop)
-#pragma config(Motor,  port4,           backright,    tmotorNormal, openLoop, reversed)
-#pragma config(Motor,  port5,           frontright,     tmotorNormal, openLoop, reversed)
-#pragma config(Motor,  port6,           height,     tmotorNormal, openLoop)
+#pragma config(Motor,  port2,           rightMotor,    tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port3,           leftMotor,     tmotorNormal, openLoop)
+#pragma config(Motor,  port4,           claw,          tmotorNormal, openLoop)
 
-
-task main()
+task main ()
 {
+
   while(1 == 1)
   {
-    motor[frontleft] = vexRT[Ch3] / 2;
-    motor[frontright] = vexRT[Ch2] / 2;
-	motor[backleft] = vexRT[Ch3] / 2;
-    motor[backright] = vexRT[Ch2] / 2;
-
-    /*
-	if(vexRT[Btn6U] == 1)
-    {
-      motor[armMotor] = 40;
-    }
-    else if(vexRT[Btn6D] == 1)
-    {
-      motor[armMotor] = -40;
-    }
-    else
-    {
-      motor[armMotor] = 0;
-    }
+    motor[leftMotor]  = (vexRT[Ch2] + vexRT[Ch1])/2;
+    motor[rightMotor] = (vexRT[Ch2] - vexRT[Ch1])/2;
   }
-  */
 }
